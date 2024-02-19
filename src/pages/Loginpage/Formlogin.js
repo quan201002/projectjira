052b4a75e660a4c6ec835dd5eb02f13140606b14
hieuhuynh <hieuhuynh201416@gmail.com>
2024-02-19 19:43:51 +0700

    chinh layout login

--- src/pages/Loginpage/Formlogin.js
+++ src/pages/Loginpage/Formlogin.js
@@ -4,9 +4,9 @@ import { Input, Button } from "antd";
 import {
   FacebookOutlined,
   TwitterOutlined,
-  GoogleOutlined,
   LockOutlined,
   UserOutlined,
+  GoogleOutlined,
 } from "@ant-design/icons";
 import { withFormik } from "formik";
 import * as Yup from "yup";
@@ -33,65 +33,87 @@ const Formlogin = (props) => {
 
   return (
     <form onSubmit={handleSubmit} style={{ textAlign: "center" }}>
-      <div style={{ marginTop: "20vh", display: "flex", flexDirection: "column", alignItems: "center" }}>
-        <h3 style={{ color: "#1890ff", marginBottom: "30px" }}>
-          {props.displayName}
-        </h3>
-        <Input
-          onChange={handleChange}
-          value={values.email}
-          size="large"
-          name="email"
-          placeholder="Email"
-          prefix={<UserOutlined />}
-          style={{ marginBottom: "20px", width: "300px" }}
-        />
-        {errors.email && touched.email ? <div>{errors.email}</div> : null}
-        <Input
-          type="password"
-          onChange={handleChange}
-          value={values.passWord}
-          size="large"
-          name="passWord"
-          placeholder="Password"
-          prefix={<LockOutlined />}
-          style={{ marginBottom: "20px", width: "300px" }}
-        />
-        {errors.passWord && touched.passWord ? (
-          <div>{errors.passWord}</div>
-        ) : null}
-        <Button
-          htmlType="submit"
-          size="large"
-          className="btn btn-primary"
-          style={{ width: "300px", backgroundColor: "#1890ff", borderColor: "#1890ff" }}
-        >
-          Login
-        </Button>
-        <div className="social mt-5" style={{ marginTop: "20px" }}>
-          <Button
-            className="bg-primary me-3"
-            type="primary"
-            shape="circle"
-            icon={<FacebookOutlined />}
-            size={"large"}
-          ></Button>
-
-          <Button
-            className="bg-primary me-3"
-            type="primary"
-            shape="circle"
-            icon={<GoogleOutlined />}
-            size={"large"}
-          ></Button>
-
-          <Button
-            className="bg-primary"
-            type="primary"
-            shape="circle"
-            icon={<TwitterOutlined />}
-            size={"large"}
-          ></Button>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          height: "100vh",
+        }}
+      >
+        <div style={{ width: "50%" }}>
+          {/* Phần hiển thị hình ảnh */}
+          <img
+            src="/HinhNenLogin.png"
+            alt="Login Image"
+            style={{ width: "100%", height: "auto" }}
+          />
+        </div>
+        <div style={{ width: "50%" }}>
+          {/* Phần hiển thị form đăng nhập */}
+          <div style={{ width: "50%", margin: "0 auto" }}>
+            <h3 style={{ color: "#1890ff", marginBottom: "30px" }}>
+              {props.displayName}
+            </h3>
+            <Input
+              onChange={handleChange}
+              value={values.email}
+              size="large"
+              name="email"
+              placeholder="Email"
+              prefix={<UserOutlined />}
+              style={{ marginBottom: "20px" }}
+            />
+            {errors.email && touched.email ? <div>{errors.email}</div> : null}
+            <Input
+              type="password"
+              onChange={handleChange}
+              value={values.passWord}
+              size="large"
+              name="passWord"
+              placeholder="Password"
+              prefix={<LockOutlined />}
+              style={{ marginBottom: "20px" }}
+            />
+            {errors.passWord && touched.passWord ? (
+              <div>{errors.passWord}</div>
+            ) : null}
+            <Button
+              htmlType="submit"
+              size="large"
+              className="btn btn-primary"
+              style={{
+                width: "100%",
+                backgroundColor: "#1890ff",
+                borderColor: "#1890ff",
+              }}
+            >
+              Login
+            </Button>
+            <div className="social mt-5" style={{ marginTop: "20px" }}>
+              <Button
+                className="bg-primary me-3"
+                type="primary"
+                shape="circle"
+                icon={<FacebookOutlined />}
+                size={"large"}
+              ></Button>
+              <Button
+                className="bg-primary me-3"
+                type="primary"
+                shape="circle"
+                icon={<TwitterOutlined />}
+                size={"large"}
+              ></Button>
+              <Button
+                className="bg-primary"
+                type="primary"
+                shape="circle"
+                icon={<GoogleOutlined />}
+                size={"large"}
+              ></Button>
+            </div>
+          </div>
         </div>
       </div>
     </form>
