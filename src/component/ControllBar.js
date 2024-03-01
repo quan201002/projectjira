e9f98f1d510c93d0a9d5e90bb3f0fb746ef11ae9
quan201002 <duongminhquan313@gmail.com>
2024-03-01 21:47:47 +0700

    quan3/1

--- src/component/ControllBar.js
+++ src/component/ControllBar.js
@@ -9,6 +9,7 @@ import React, { useState } from "react";
 import { Button, Divider, Layout, Menu, theme } from "antd";
 import { useDispatch } from "react-redux";
 import { NavLink } from "react-router-dom";
+import FormCreateTask from "../HOC/JiraCloneHOC/FormCreateTask";
 
 const { Content, Sider } = Layout;
 
@@ -22,72 +23,83 @@ const ControllBar = () => {
   } = theme.useToken();
   return (
     <>
-   
       <Layout style={{ height: "100%" }}>
         <div className="container1">
-        <div className="sider-container">
-          <Sider
-            style={{ height: "100%" }}
-            trigger={null}
-            collapsible
-            collapsed={collapsed}
-          >
-            <Button
-              type="text"
-              icon={<MenuOutlined style={{ color: "white" }} />}
-              onClick={() => setCollapsed(!collapsed)}
+          <div className="sider-container">
+            <Sider
+              style={{ height: "100%" }}
+              trigger={null}
+              collapsible
+              collapsed={collapsed}
+            >
+              <Button
+                type="text"
+                icon={<MenuOutlined style={{ color: "white" }} />}
+                onClick={() => setCollapsed(!collapsed)}
+                style={{
+                  fontSize: "16px",
+                  width: "100%",
+                  height: 64,
+                  padding: "18px",
+                  paddingRight: "30px",
+                  display: "flex",
+                  justifyContent: "right",
+                  alignItems: "center",
+                }}
+              />
+              <Menu
+                theme="dark"
+                mode="inline"
+                items={[
+                  {
+                    key: "1",
+                    icon: <PlusOutlined />,
+                    label: "Create task",
+                    onClick: () => {
+                      dispatch({
+                        type: "OPEN_FORM_CREATE_TASK",
+                        ComponentContentDrawer: FormCreateTask,
+                        open: true,
+                      });
+                    },
+                  },
+                  {
+                    key: "2",
+                    icon: <SearchOutlined />,
+                    label: "Search",
+                  },
+                ]}
+              />
+            </Sider>
+            <Sider
+              className="d-flex justify-content-center "
               style={{
-                fontSize: "16px",
-                width: "100%",
-                height: 64,
-                padding: "18px",
-                paddingRight: "30px",
-                display: "flex",
-                justifyContent: "right",
-                alignItems: "center",
+                background: colorBgContainer,
+                height: "100vh",
               }}
-            />
-            <Menu
-              theme="dark"
-              mode="inline"
-              items={[
-                {
-                  key: "1",
-                  icon: <PlusOutlined />,
-                  label: "Create task",
-                },
-                {
-                  key: "2",
-                  icon: <SearchOutlined />,
-                  label: "Search",
-                },
-              ]}
-            />
-          </Sider>
-          <Sider
-            className="d-flex justify-content-center "
-            style={{
-              background: colorBgContainer,
-              height: "100vh",
-            }}
-            width={200}
-          >
-            
-            <p>
-              <SettingFilled />
-              <NavLink className="  menu-item" to="/createproject">
-                Createproject
-              </NavLink>
-            </p>
-            <p>
-              <SettingFilled />
-              <NavLink className="  menu-item" to="/projectmanagement">
-                Project management
-              </NavLink>
-            </p>
-            <Divider type="horizontal"/>
-          </Sider>
-        </div>
+              width={200}
+            >
+              <p>
+                <SettingFilled />
+                <NavLink className="  menu-item" to="/createproject">
+                  Createproject
+                </NavLink>
+              </p>
+              <p>
+                <SettingFilled />
+                <NavLink className="  menu-item" to="/projectmanagement">
+                  Project management
+                </NavLink>
+              </p>
+              <p>
+                <SettingFilled />
+                <NavLink className=" menu-item" to="/Home">
+                  User management
+                </NavLink>
+              </p>
+              <Divider type="horizontal" />
+            </Sider>
+          </div>
         </div>
       </Layout>
     </>
--- src/pages/Homepage/Homepage.js
+++ src/pages/Homepage/Homepage.js
@@ -1,15 +1,54 @@
 import React from "react";
+import "./Homepage.css";
 import { useSelector } from "react-redux";
-
+import { Layout, theme } from "antd";
+const { Header, Content, Footer, Sider } = Layout;
 export default function Homepage() {
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
   const userLogin = useSelector(
     (state) => state.UserLoginCyberBugsReducer.userLogin
   );
   console.log("user", userLogin);
   return (
-    <div className="content-container">
-      {userLogin?.name}
-      <img src={userLogin?.avatar}></img>
+    <div className="home-container">
+      <Layout>
+        <Header
+          style={{
+            padding: 0,
+            background: colorBgContainer,
+            height: "auto",
+            borderRadius: borderRadiusLG,
+          }}
+        >
+          <div className="text-right  ">
+            <span>
+              Hello, {userLogin.name}{" "}
+              <img
+                style={{ borderRadius: "50%", backgroundSize: "80%" }}
+                src={userLogin.avatar}
+              ></img>
+            </span>
+          </div>
+        </Header>
+
+        <Content
+          style={{
+            margin: "24px 16px 0",
+            height: "auto",
+          }}
+        >
+          <div
+            style={{
+              padding: 24,
+              minHeight: 360,
+              background: colorBgContainer,
+              borderRadius: borderRadiusLG,
+            }}
+          ></div>
+        </Content>
+      </Layout>
     </div>
   );
 }
